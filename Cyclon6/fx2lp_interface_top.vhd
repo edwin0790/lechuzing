c5069267ea2086cc9a07bc028f0e73b81e425cb0
edwin0790 <e.b.1990@ieee.org>
2017-07-13 16:29:47 -0300

    maquina de estados que compila. Falta hacer la interfaz de memoria y hacia el resto del chip.

--- Cyclon6/fx2lp_interface_top.vhd
+++ Cyclon6/fx2lp_interface_top.vhd
@@ -12,10 +12,18 @@
 --------------------------------------------------------------------------------
 library ieee;
 use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
+use ieee.std_logic_arith.all;
+use ieee.std_logic_unsigned.all;
+library unisim;
+use unisim.vcomponents.all;
 
 entity fx2lp_interface_top is
   generic(
+  -- address are:
+  --  * EP2 = "00"
+  --  * EP4 = "01"
+  --  * EP6 = "10"
+  --  * EP8 = "11"
   constant in_ep_addr:	std_logic_vector(1 downto 0) := "00";
   constant out_ep_addr:	std_logic_vector(1 downto 0) := "11";
   constant port_width: integer := 15
@@ -27,10 +35,10 @@ entity fx2lp_interface_top is
     slrd    : out   std_logic;                              -- señal de lectura
     slwr    : out   std_logic;                              -- señal de escritura
 
-    flaga   : in    std_logic;                              -- flags defindo por...leer en el firmware
-    flagb   : in    std_logic;
-    flagc   : in    std_logic;
-    flagd   : in    std_logic;
+    flaga   : in    std_logic;                              -- EP2_full
+    flagb   : in    std_logic;                              -- EP2_empty
+    flagc   : in    std_logic;                              -- EP8_full
+    flagd   : in    std_logic;                              -- EP8_empty
     sloe    : out   std_logic;                              -- señal de habilitación de salida
 
     pktend  : out   std_logic;
@@ -42,7 +50,7 @@ entity fx2lp_interface_top is
       -- FX2LP, por lo que se provee entrada y salida de reloj y un pin de
       -- seleccion, clk_src.
       -- En caso de reloj interno, el reloj externo será el de la Mojo, es decir
-      -- de 50 MHz, para que el pll pueda obtener uina señal de 48 MHz
+      -- de 50 MHz, para que el pll pueda obtener una señal de 48 MHz
       -- clk_src '1' => reloj interno se emite por clk_out.
       -- clk_src '0' => timing dado por reloj externo
     clk_in  : in    std_logic;                              -- entrada de reloj
@@ -50,7 +58,8 @@ entity fx2lp_interface_top is
     clk_src : in    std_logic;                              -- seleccion de modo de reloj.
 
     -- señales que se comunican desde y hacia el sistema
-    reset   : in    std_logic                               -- fundamentalmente para sincronización
+    reset   : in    std_logic;                              -- fundamentalmente para sincronización
+    send_req: in    std_logic                               -- pedido de envío de datos
   );
 end fx2lp_interface_top;
 
@@ -73,22 +82,33 @@ architecture fx2lp_interface_arq of fx2lp_interface_top is
   signal slwr_int, slrd_int, sloe_int, done_int :   std_logic;
   signal faddr_int                              :   std_logic_vector(1 downto 0);
   signal fdata_out, fdata_in                    :   std_logic_vector(port_width downto 0);
+  signal read_empty_flag, read_full_flag        :   std_logic;
+  signal write_empty_flag, write_full_flag      :   std_logic;
+  signal write_req                              :   std_logic;
 
-  -- Maquinas de Estados:
-    --Por simplicidad, se plantean tres MAE's: una general que define si hacemos
-    -- una lectura, una escritura, o estamos a la espera de una evento.
-    --Cuando este evento ocurre, se dispara una de las otras dos MAE's, ya sea,
-    -- para leer o escribir datos.
-      --Maquina de lectura
-      type read_states is (read_idle, read_addr, read_wait_empty, read_read, read_end);
-      signal curr_read_state, next_read_state : read_states;
-      --Maquina de escritura
-      type write_states is (write_idle, write_addr, write_no_full, write_write, write_end);
-      signal curr_write_state, nex_write_state : write_states;
+  -- Maquinas de Estados: xc6slx9-2tqg144
       --Maquina global
-      type global_states is (idle, reading, writing);
-      signal global_state : global_states := idle;
+      type global_states is
+      (
+        idle,
+        read_addr, read_wait_empty, read_read, read_end,
+        write_addr, write_no_full, write_write, write_end
+      );
+      signal curr_state, next_state : global_states := idle;
 begin
+
+  oddr_y : ODDR2 	                                           -- clk out buffer
+  port map
+  (
+   D0 	=> '1',
+   D1 	=> '0',
+   CE 	=> '1',
+   C0	=> pll_180,
+   C1	=> (not pll_180),
+   R  	=> '0',
+   S  	=> '0',
+   Q  	=> clk_out
+  );
 
   pll : clk_wiz_v3_6
   port map(
@@ -102,33 +122,116 @@ begin
   );
 
   --selección de reloj
-  sys_clk <=  clk_in  when clk_src = '0' else
-              pll_180;
+  -- sys_clk <=  --clk_in  when clk_src = '0' else --esto se debe seleccionar al compilar. No se puede hacer por hardware
+  --             pll_180;
 
-  clk_out <=  clk_in  when  clk_src = '0' else
-              pll_0;
+  -- clk_out <=  --clk_in  when  clk_src = '0' else --idem arriba
+  --             pll_0;
 
   --conexiones de señales internas hacia el exterior
-  slwr        <= slwr_int;
-  slrd        <= slrd_int;
-  sloe        <= sloe_int;
-  faddr       <= faddr_int;
-  fdata       <= fdata_out;
-  pktend      <= '1';
-  done        <= done_int;
+  slwr   <= slwr_int;
+  slrd   <= slrd_int;
+  sloe   <= sloe_int;
+  faddr  <= faddr_int;
+  pktend <= '1';
+  -- done   <= done_int; --for debug
+
+  write_full_flag  <= flaga;
+  write_empty_flag  <= flagb;
+  read_full_flag  <= flagc;
+  read_empty_flag  <= flagd;
+
+  write_req <= send_req;
 
   -- control signaling
-  faddr_int <=  out_ep_addr  when global_state = reading else
-                in_ep_addr;
+  with curr_state select
+    faddr_int <=  out_ep_addr when read_addr | read_wait_empty | read_read | read_end,
+                  in_ep_addr  when write_addr | write_no_full | write_write | write_end,
+                  (others => 'Z') when others;
 
-  slwr_int <= '0' when curr_write_state = write_write else
+  slwr_int <= '0' when curr_state = write_write else
               '1';
 
-  slrd_int <= '0' when curr_read_state = read_read else
+  slrd_int <= '0' when curr_state = read_read else
               '1';
 
-  sloe_int <= '0' when global_state = reading else
-              '1';
+  with curr_state select
+    sloe_int <= '0' when read_addr | read_wait_empty | read_read | read_end,
+                '1' when others;
+
+  fdata <= fdata_out when curr_state = write_write else (others => 'Z');
+
+  fdata_in <= fdata when curr_state = read_read else (others => 'Z');
+--              (others => '0');
 
   -- Implementacion de las maquinas de estado
+  fsm: process(curr_state, write_full_flag, read_empty_flag)
+    begin
+      case curr_state is
+        when idle =>
+          if(read_empty_flag = '1')then
+            next_state <= read_addr;
+          elsif((write_full_flag = '1') and (write_req = '1'))then
+            next_state <= write_addr;
+          else
+            next_state <= idle;
+          end if;
+
+        when read_addr =>
+          next_state <= read_wait_empty;
+
+        when read_wait_empty =>
+          if(read_empty_flag = '1')then
+            next_state <= read_read;
+          else
+            next_state <= read_wait_empty;
+          end if;
+
+        when read_read =>
+          next_state <= read_end;
+
+        when read_end =>
+          if(read_empty_flag = '1')then
+            next_state <= read_read;
+          else
+            next_state <= idle;
+          end if;
+
+        when write_addr =>
+          next_state <= write_no_full;
+
+        when write_no_full =>
+          if(read_empty_flag = '0')then
+            next_state <= idle;
+          elsif(write_full_flag = '1')then
+            next_state <= write_write;
+          else
+            next_state <= write_no_full;
+          end if;
+
+        when write_write =>
+          next_state <= write_end;
+
+        when write_end =>
+          if((write_full_flag = '1') and (read_empty_flag = '1'))then
+            next_state <= write_write;
+          else
+            next_state <= idle;
+          end if;
+
+        when others =>
+          next_state <= idle;
+      end case;
+    end process fsm;
+
+    -- reloj
+    global_fsm_clk: process (sys_clk, reset)
+    begin
+      if(reset = '1')then
+        curr_state <= idle;
+      elsif(rising_edge(sys_clk))then
+        curr_state <= next_state;
+      end if;
+    end process global_fsm_clk;
+
 end fx2lp_interface_arq;
