d2cd5bec726811064b4ad31c3df2c1d64bd245df
edwin0790 <e.b.1990@ieee.org>
2019-05-16 16:36:51 -0300

    Beamer: USB completo y trabajando en FX2LP

--- Presentacion/codes/bridge.c
+++ Presentacion/codes/bridge.c
@@ -15,38 +15,39 @@ extern BOOL   Selfpwr;
 #define BTN_ADDR		0x20
 #define LED_ADDR		0x21
 
-BYTE xdata Digit[] = { 0xc0, 0xf9, 0xa4, 0xb0, 0x99, 0x92, 0x82, 0xf8, 0x80, 0x98, 0x88, 0x83, 0xc6, 0xa1, 0x86, 0x8e };
+BYTE xdata Digit[] = {0xc0, 0xf9, 0xa4, 0xb0, 0x99, 0x92, 0x82,
+		0xf8, 0x80, 0x98, 0x88, 0x83, 0xc6, 0xa1, 0x86,
+		0x8e};
 
 // Dummy-Read these values to turn LED's on and off
-//xdata volatile const BYTE D2ON		_at_ 0x8800;
+//xdata volatile const BYTE D2ON	_at_ 0x8800;
 //xdata volatile const BYTE D2OFF	_at_ 0x8000;
-//xdata volatile const BYTE D3ON		_at_ 0x9800;
+//xdata volatile const BYTE D3ON	_at_ 0x9800;
 //xdata volatile const BYTE D3OFF	_at_ 0x9000;
-//xdata volatile const BYTE D4ON		_at_ 0xA800;
+//xdata volatile const BYTE D4ON	_at_ 0xA800;
 //xdata volatile const BYTE D4OFF	_at_ 0xA000;
-//xdata volatile const BYTE D5ON		_at_ 0xB800;
+//xdata volatile const BYTE D5ON	_at_ 0xB800;
 //xdata volatile const BYTE D5OFF	_at_ 0xB000;
 
-BYTE    Configuration;      // Current configuration
+BYTE    Configuration;      	// Current configuration
 BYTE    AlternateSetting = 0;   // Alternate settings
 
-//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------
 // Task Dispatcher hooks
 //   The following hooks are called by the task dispatcher.
-//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------
 
 WORD mycount = 0;
 WORD blinktime = 0;
 BYTE inblink = 0x00;
 BYTE outblink = 0x00;
-WORD blinkmask = 0;			// HS/FS blink rate
+WORD blinkmask = 0;		// HS/FS blink rate
 
 BYTE refresh = 0;
 
-void TD_Init(void)             // Called once at startup
+void TD_Init(void)             	// Called once at startup
 {
 	BYTE dum;
-//	WORD i;
 	
 	// turn off the 4 LED's
 	dum = D2OFF;
@@ -55,29 +56,41 @@ void TD_Init(void)             // Called once at startup
 	dum = D5OFF;
 
    	// set the CPU clock to 48MHz
-	//CPUCS = ((CPUCS & ~bmCLKSPD) | bmCLKSPD1); // 48 MHz //comented 'cos are configured by Serial_Init
+	//CPUCS = ((CPUCS & ~bmCLKSPD) | bmCLKSPD1); 
+	// 48 MHz //comented 'cos are configured by Serial_Init
 	FX2LPSerial_Init();
 	FX2LPSerial_XmitString("Serial Port Initialized");
 	FX2LPSerial_XmitChar('\n');
 	FX2LPSerial_XmitChar('\n');
 	
 	// set the slave FIFO interface to 48MHz
-   		//set s-fifo to internal clk, no_output, no_inverted clk, no_async, no_gstate, slavefifo(11) = 0xC3
-	IFCONFIG = 0xcb;/*/0xCB; *///para hacerlo async. Error de diseño. Corregir en VHDL previamente
+   		//set s-fifo to internal clk, no_output,
+		//no_inverted clk, no_async, no_gstate, 
+		//slavefifo(11) = 0xC3
+	IFCONFIG = 0xcb;//para hacerlo async.
+			//Error de diseno.
+			//Corregir en VHDL previamente
 
 	SYNCDELAY;
-	REVCTL = 0x00; /*Chip Revision Control Register: poniendo esto en 0x01 me deja manipular los paquetes, pero debo mover paquete por paquete a 
-										cada buffer... no logro tomar ningún tipo de flag y mucho menos pude mover el buffer a la memoria fifo
-										Por otro lado, poniendo el bit 0x02 logro desactivar el auto-armado de los endopints y puedo cambiar de modo autoout a manualout
-										sin necesidad de perder datos.*/
+	REVCTL = 0x00; /*Chip Revision Control Register: poniendo
+			esto en 0x01 me deja manipular los
+			paquetes, pero debo mover paquete por
+			paquete a cada buffer... 
+			no logro tomar ningun tipo de flag y mucho
+			menos pude mover el buffer a la memoria fifo
+
+			Por otro lado, poniendo el bit 0x02 logro
+			desactivar el auto-armado de los endopints
+			y puedo cambiar de modo autoout a manualout
+			sin necesidad de perder datos.*/
 	SYNCDELAY;
 
 	//Pin Flags Configuration
-	PINFLAGSAB = 0xBC;  // FLAGA <- EP2 Full Flag
-						// FLAGD <- EP2 Empty Flag
+	PINFLAGSAB = 0xBC;	// FLAGA <- EP2 Full Flag
+				// FLAGD <- EP2 Empty Flag
 	SYNCDELAY;
 	PINFLAGSCD = 0x8F;	// FLAGC <- EP8 Full Flag
-						// FLAGB <- EP8 Empty Flag
+				// FLAGB <- EP8 Empty Flag
 
 	EP1OUTCFG = 0xA0;
 	SYNCDELAY;
@@ -87,9 +100,9 @@ void TD_Init(void)             // Called once at startup
 	SYNCDELAY;
 	EP6CFG &= 0x7F;
 	SYNCDELAY;
-	EP8CFG = 0xA0; //EP8 is DIR=OUT, TYPE=BULK, SIZE=512, BUF=2x
+	EP8CFG = 0xA0;	//EP8: DIR=OUT, TYPE=BULK, SIZE=512, BUF=2x
 	SYNCDELAY;
-	EP2CFG = 0xD2;  // EP2 is DIR=IN, TYPE=ISOC, SIZE=1024, BUF=3x EP2CFG@e612 --> size = 512, buf = 2x
+	EP2CFG = 0xD2;	//EP2: DIR=IN, TYPE=ISOC, SIZE=1024, BUF=3x 
 	SYNCDELAY;
 
 	FIFORESET = 0x80;
@@ -111,7 +124,7 @@ void TD_Init(void)             // Called once at startup
 	SYNCDELAY;
 
 	//setting on auto mode. rising edge is necessary
-	EP8FIFOCFG = 0x11;// & ~bmAUTOOUT; //at the end, auto mode is setted off
+	EP8FIFOCFG = 0x11;
 	SYNCDELAY;
 	EP2FIFOCFG = 0x0D;
 	SYNCDELAY;
@@ -126,57 +139,59 @@ void TD_Init(void)             // Called once at startup
 	FX2LPSerial_XmitString("Bridge Configured\n");
 }
 
-void TD_Poll(void)             // Called repeatedly while the device is idle  
+void TD_Poll(void)     // Called repeatedly while the device is idle
 {
 	BYTE dum;
-	WORD i;
 		
-		if(EP8FIFOFLGS & bmBIT1)//ep8 fifo empty
-		{
-			dum = D4ON;
-		}
-		else
-		{
-			dum = D4OFF;
-		}
+	if(EP8FIFOFLGS & bmBIT1)//ep8 fifo empty
+	{
+		dum = D4ON;
+	}
+	else
+	{
+		dum = D4OFF;
+	}
 
-		if(EP8FIFOFLGS & bmBIT0)//ep8 fifo full//(EP2468STAT & bmBIT6)//ep8 empty
-		{
-			dum = D3ON;
-		}
-		else
-		{
-			dum = D3OFF;
-		}
+	if(EP8FIFOFLGS & bmBIT0)//ep8 fifo full//
+	//(EP2468STAT & bmBIT6)//ep8 empty
+	{
+		dum = D3ON;
+	}
+	else
+	{
+		dum = D3OFF;
+	}
 
-		if(EP2FIFOFLGS & bmBIT1)//ep2 fifo empty
-		{
-			dum = D2ON;
-		}
-		else
-		{
-			dum = D2OFF;
-		}
+	if(EP2FIFOFLGS & bmBIT1)//ep2 fifo empty
+	{
+		dum = D2ON;
+	}
+	else
+	{
+		dum = D2OFF;
+	}
 
 }
 
 
 
 
-BOOL TD_Suspend(void)          // Called before the device goes into suspend mode
+BOOL TD_Suspend(void)	//Called before the device goes into
+			//suspend mode
 {
    return(TRUE);
 }
 
-BOOL TD_Resume(void)          // Called after the device resumes
+BOOL TD_Resume(void)	// Called after the device resumes
 {
    return(TRUE);
 }
 
-//-----------------------------------------------------------------------------
+//------------------------------------------------------------------
 // Device Request hooks
-//   The following hooks are called by the end point 0 device request parser.
-//-----------------------------------------------------------------------------
+//   The following hooks are called by the end point 0 device
+//   request parser.
+//------------------------------------------------------------------
 
 BOOL DR_GetDescriptor(void)
 {
@@ -184,37 +199,41 @@ BOOL DR_GetDescriptor(void)
 	return(TRUE);
 }
 
-BOOL DR_SetConfiguration(void)   // Called when a Set Configuration command is received
+BOOL DR_SetConfiguration(void)	//Called when a Set Configuration
+				//command is received
 {
 	FX2LPSerial_XmitString("Setting Config\n\n");
 	Configuration = SETUPDAT[2];
-	return(TRUE);            // Handled by user code
+	return(TRUE);		// Handled by user code
 }
 
-BOOL DR_GetConfiguration(void)   // Called when a Get Configuration command is received
+BOOL DR_GetConfiguration(void)	//Called when a Get Configuration
+				//command is received
 {
 	FX2LPSerial_XmitString("Getting Config\n\n");
 	EP0BUF[0] = Configuration;
 	EP0BCH = 0;
 	EP0BCL = 1;
-	return(TRUE);            // Handled by user code
+	return(TRUE);		// Handled by user code
 }
 
-BOOL DR_SetInterface(void)       // Called when a Set Interface command is received
+BOOL DR_SetInterface(void)	//Called when a Set Interface
+				//command is received
 {
 	FX2LPSerial_XmitString("Setting Interface\n\n");
 
 	AlternateSetting = SETUPDAT[2];
-  return(TRUE);            // Handled by user code
+  	return(TRUE);		// Handled by user code
 }
 
-BOOL DR_GetInterface(void)       // Called when a Set Interface command is received
+BOOL DR_GetInterface(void)	//Called when a Set Interface
+				//command is received
 {
 	FX2LPSerial_XmitString("Getting Interface\n\n");
-   EP0BUF[0] = AlternateSetting;
-   EP0BCH = 0;
-   EP0BCL = 1;
-   return(TRUE);            // Handled by user code
+	EP0BUF[0] = AlternateSetting;
+	EP0BCH = 0;
+	EP0BCL = 1;
+	return(TRUE);		// Handled by user code
 }
 
 BOOL DR_GetStatus(void)
@@ -237,22 +256,23 @@ BOOL DR_SetFeature(void)
 
 BOOL DR_VendorCmnd(void)
 {
-   return(TRUE);
+	return(TRUE);
 }
 
-//-----------------------------------------------------------------------------
+//------------------------------------------------------------------
 // USB Interrupt Handlers
-//   The following functions are called by the USB interrupt jump table.
-//-----------------------------------------------------------------------------
+//   The following functions are called by the USB interrupt jump
+//   table.
+//------------------------------------------------------------------
 
 // Setup Data Available Interrupt Handler
 void ISR_Sudav(void) interrupt 0
 {
 
 	FX2LPSerial_XmitString("SUDAv ISR\n");
-	GotSUD = TRUE;            // Set flag
+	GotSUD = TRUE;		// Set flag
 	EZUSB_IRQ_CLEAR();
-	USBIRQ = bmSUDAV;         // Clear SUDAV IRQ
+	USBIRQ = bmSUDAV;	// Clear SUDAV IRQ
 }
 
 // Setup Token Interrupt Handler
@@ -260,14 +280,14 @@ void ISR_Sutok(void) interrupt 0
 {
 	FX2LPSerial_XmitString("SUTok ISR\n");
 	EZUSB_IRQ_CLEAR();
-	USBIRQ = bmSUTOK;         // Clear SUTOK IRQ
+	USBIRQ = bmSUTOK;	// Clear SUTOK IRQ
 }
 
 void ISR_Sof(void) interrupt 0
 {
 	BYTE dum;
 	EZUSB_IRQ_CLEAR();
-	USBIRQ = bmSOF;            // Clear SOF IRQ
+	USBIRQ = bmSOF;		// Clear SOF IRQ
 	blinktime++;
 	if(blinktime&blinkmask)
 		dum=D5OFF;	// ~1/sec blinking LED
@@ -284,132 +304,48 @@ void ISR_Ures(void) interrupt 0
 {
 	BYTE dum;
 
-	// Whenever we get a USB Reset, we should revert to full speed mode
+	// Whenever we get a USB Reset, we should revert to
+	// full speed mode
 	FX2LPSerial_XmitString("URst ISR\n");
 	pConfigDscr = pFullSpeedConfigDscr;
-	((CONFIGDSCR xdata *) pConfigDscr)->type = CONFIG_DSCR;
+	((CONFIGDSCR xdata *)pConfigDscr)->type = CONFIG_DSCR;
 	pOtherConfigDscr = pHighSpeedConfigDscr;
-	((CONFIGDSCR xdata *) pOtherConfigDscr)->type = OTHERSPEED_DSCR;
+	((CONFIGDSCR xdata*)pOtherConfigDscr)->type = 
+						OTHERSPEED_DSCR;
    
-   EZUSB_IRQ_CLEAR();
-   USBIRQ = bmURES;         // Clear URES IRQ
-   dum = D2OFF;				// Turn off high-speed LED
-   blinkmask = 0x0400;		// 2 sec period for FS
+	EZUSB_IRQ_CLEAR();
+	USBIRQ = bmURES;	// Clear URES IRQ
+	dum = D2OFF;		// Turn off high-speed LED
+	blinkmask = 0x0400;	// 2 sec period for FS
 }
 
 void ISR_Susp(void) interrupt 0
 {
-   Sleep = TRUE;
-   EZUSB_IRQ_CLEAR();
-   USBIRQ = bmSUSP;
+	Sleep = TRUE;
+	EZUSB_IRQ_CLEAR();
+	USBIRQ = bmSUSP;
 }
 
 void ISR_Highspeed(void) interrupt 0
 {
-   blinkmask = 0x1000;		// 1 sec period for HS
-   if (EZUSB_HIGHSPEED())
-   {
-      pConfigDscr = pHighSpeedConfigDscr;
-      ((CONFIGDSCR xdata *) pConfigDscr)->type = CONFIG_DSCR;
-      pOtherConfigDscr = pFullSpeedConfigDscr;
-      ((CONFIGDSCR xdata *) pOtherConfigDscr)->type = OTHERSPEED_DSCR;
-   }
-   else
-   {
-      pConfigDscr = pFullSpeedConfigDscr;
-      pOtherConfigDscr = pHighSpeedConfigDscr;
-   }
+	blinkmask = 0x1000;	// 1 sec period for HS
+	if (EZUSB_HIGHSPEED())
+	{
+		pConfigDscr = pHighSpeedConfigDscr;
+		((CONFIGDSCR xdata *) pConfigDscr)->type =
+						CONFIG_DSCR;
+		pOtherConfigDscr = pFullSpeedConfigDscr;
+		((CONFIGDSCR xdata *) pOtherConfigDscr)->type =
+						OTHERSPEED_DSCR;
+	}
+	else
+	{
+	pConfigDscr = pFullSpeedConfigDscr;
+	pOtherConfigDscr = pHighSpeedConfigDscr;
+	}
 
-   EZUSB_IRQ_CLEAR();
-   USBIRQ = bmHSGRANT;
-}
-void ISR_Ep0ack(void) interrupt 0
-{
-}
-void ISR_Stub(void) interrupt 0
-{
-}
-void ISR_Ep0in(void) interrupt 0
-{
-}
-void ISR_Ep0out(void) interrupt 0
-{
-}
-void ISR_Ep1in(void) interrupt 0
-{
-}
-void ISR_Ep1out(void) interrupt 0
-{
-}
-
-void ISR_Ep2inout(void) interrupt 0
-{
-}
-void ISR_Ep4inout(void) interrupt 0
-{
-}
-void ISR_Ep6inout(void) interrupt 0
-{
-}
-void ISR_Ep8inout(void) interrupt 0
-{
-}
-void ISR_Ibn(void) interrupt 0
-{
-}
-void ISR_Ep0pingnak(void) interrupt 0
-{
-}
-void ISR_Ep1pingnak(void) interrupt 0
-{
-}
-void ISR_Ep2pingnak(void) interrupt 0
-{
-}
-void ISR_Ep4pingnak(void) interrupt 0
-{
-}
-void ISR_Ep6pingnak(void) interrupt 0
-{
-}
-void ISR_Ep8pingnak(void) interrupt 0
-{
-}
-void ISR_Errorlimit(void) interrupt 0
-{
-}
-void ISR_Ep2piderror(void) interrupt 0
-{
-}
-void ISR_Ep4piderror(void) interrupt 0
-{
-}
-void ISR_Ep6piderror(void) interrupt 0
-{
-}
-void ISR_Ep8piderror(void) interrupt 0
-{
-}
-void ISR_Ep2pflag(void) interrupt 0
-{
-}
-void ISR_Ep4pflag(void) interrupt 0
-{
-}
-void ISR_Ep6pflag(void) interrupt 0
-{
-}
-void ISR_Ep8pflag(void) interrupt 0
-{
-}
-void ISR_Ep2eflag(void) interrupt 0
-{
-}
-void ISR_Ep4eflag(void) interrupt 0
-{
-}
-void ISR_Ep6eflag(void) interrupt 0
-{
+	EZUSB_IRQ_CLEAR();
+	USBIRQ = bmHSGRANT;
 }
 void ISR_Ep8eflag(void) interrupt 0
 {
@@ -417,18 +353,9 @@ void ISR_Ep8eflag(void) interrupt 0
 	EP8FIFOIRQ = 0x02;
 	FX2LPSerial_XmitHex2(EP8BCH);
 	FX2LPSerial_XmitHex2(EP8BCL);
-	FX2LPSerial_XmitString("\nEP8: Estoy vacío\n");
+	FX2LPSerial_XmitString("\nEP8: Estoy vacio\n");
 
 
-}
-void ISR_Ep2fflag(void) interrupt 0
-{
-}
-void ISR_Ep4fflag(void) interrupt 0
-{
-}
-void ISR_Ep6fflag(void) interrupt 0
-{
 }
 void ISR_Ep8fflag(void) interrupt 0
 {
@@ -436,9 +363,4 @@ void ISR_Ep8fflag(void) interrupt 0
 	EP8FIFOIRQ = 0x01;
 	FX2LPSerial_XmitString("EP8: Estoy lleno\n");
 }
-void ISR_GpifComplete(void) interrupt 0
-{
-}
-void ISR_GpifWaveform(void) interrupt 0
-{
-}
+
