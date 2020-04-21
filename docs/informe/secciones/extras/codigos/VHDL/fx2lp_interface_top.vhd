81a79835a1951e94b48bb287c441d4757d05d0af
edwin0790 <edwin0790@gmail.com>
2020-04-21 11:35:43 -0300

    Terminado Apendice con VHDL
    Terminé de corregir el idioma de todos los archivos de VHDL

--- docs/informe/secciones/extras/codigos/VHDL/fx2lp_interface_top.vhd
+++ docs/informe/secciones/extras/codigos/VHDL/fx2lp_interface_top.vhd
@@ -5,163 +5,145 @@ library unisim;
 use unisim.vcomponents.all;
 
 entity fx2lp_interface_top is
-  generic(
-  -- addresses are:|
-  --  * EP8 = "11"
-  --  * EP2 = "00"
-  constant in_ep_addr:	std_logic_vector(1 downto 0) := "00";
-  constant out_ep_addr:	std_logic_vector(1 downto 0) := "11";
-  constant port_width: integer := 16
-  );
-  port(
-    -- senales que van y vienen desde y hacia el FX2LP
-    fdata   : inout std_logic_vector(port_width-1 downto 0);  -- entrada y salida de datos FIFO
-    faddr   : out   std_logic_vector(1 downto 0);           -- canal FIFO
-    slrd    : out   std_logic;                              -- senal de lectura
-    slwr    : out   std_logic;                              -- senal de escritura
+	generic(
+		-- direcciones EP:
+		--  * EP8 = "11"
+		--  * EP2 = "00"
+		constant ent_ep_addr:std_logic_vector(1 downto 0) := "00";
+		constant sal_ep_addr:std_logic_vector(1 downto 0) := "11";
+		constant ancho_bus	:integer := 16
+	);
+	port(
+		-- senales que van y vienen desde y hacia el FX2LP
+		fdata   :inout std_logic_vector(ancho_bus-1 downto 0);
+			-- entrada y salida de datos FIFO
+		faddr   :out   std_logic_vector(1 downto 0);           
+			-- canal FIFO
+		slrd		:out	std_logic;-- senal de lectura
+		slwr		:out	std_logic;-- senal de escritura
+		flaga		:in		std_logic;-- EP2_full--->esc_flag_lleno
+		flagb		:in		std_logic;-- EP8_empty-->lec_flag_vacio
+		flagc		:in		std_logic;-- EP8_full--->lec_flag_lleno
+		flagd		:in		std_logic;-- EP2_empty-->esc_flag_vacio
+		sloe		:out	std_logic;-- senal de habilitacion de salida
+		pktend		:out	std_logic;
+		-- reloj
+		reloj_sal	:out	std_logic;-- salida de reloj
+		-- senales desde y hacia mojo
+		reloj_ent	:in		std_logic;-- entrada de reloj
+		pulsador	:in		std_logic;-- activo en bajo
+		led			:out	std_logic_vector(7 downto 0)
 
-                                                            -- EP2 streaming in (to pc)
-    flaga   : in    std_logic;                              -- EP2_full--->write_full_flag
-    flagb   : in    std_logic;                              -- EP8_empty-->read_empty_flag
-                                                            -- EP8 bulk out (from pc)
-    flagc   : in    std_logic;                              -- EP8_full--->read_full_flag
-    flagd   : in    std_logic;                              -- EP2_empty-->write_empty_flag
-    sloe    : out   std_logic;                              -- senal de habilitacion de salida
-
-    pktend  : out   std_logic;
-
-    -- reloj
-	 clk_out	: out   std_logic;										-- salida de reloj
-	 
-	 -- senales desde hacia mojo
-    clk_in  : in    std_logic;                              -- entrada de reloj
-    button  : in    std_logic;                              -- activo en bajo
-	 led		: out	std_logic_vector(7 downto 0)
-	 
-	 );
+	);
 end fx2lp_interface_top;
 
 architecture fx2lp_interface_arq of fx2lp_interface_top is
 
 	-- declaracion de componentes
 	COMPONENT fx2lp_interface
-	GENERIC(
-		constant in_ep_addr:	std_logic_vector(1 downto 0) := "00";
-		constant out_ep_addr:std_logic_vector(1 downto 0) := "11";
-		constant port_width: integer := 16
-	);
-	PORT(
-		clk : IN std_logic;
-		reset : IN std_logic;
-		flaga : IN std_logic;
-		flagb : IN std_logic;
-		flagc : IN std_logic;
-		flagd : IN std_logic;
-		send_req : IN std_logic;
-		data_to_tx : IN std_logic_vector(15 downto 0);    
-		fdata : INOUT std_logic_vector(15 downto 0);      
-		faddr : OUT std_logic_vector(1 downto 0);
-		slrd : OUT std_logic;
-		slwr : OUT std_logic;
-		sloe : OUT std_logic;
-		pktend : OUT std_logic;
-		rx_data : OUT std_logic_vector(15 downto 0)
+		GENERIC(
+			constant ent_ep_addr:std_logic_vector(1 downto 0);
+			constant sal_ep_addr:std_logic_vector(1 downto 0);
+			constant ancho_bus	:integer
+		);
+		PORT(
+			reloj : IN std_logic;
+			reset : IN std_logic;
+			enviar_datos : IN std_logic;
+			dato_a_enviar : IN std_logic_vector(15 downto 0);    
+			flaga : IN std_logic;
+			flagb : IN std_logic;
+			flagc : IN std_logic;
+			flagd : IN std_logic;
+			fdata : INOUT std_logic_vector(15 downto 0);      
+			dato_recibido : OUT std_logic_vector(15 downto 0);
+			faddr : OUT std_logic_vector(1 downto 0);
+			slrd : OUT std_logic;
+			slwr : OUT std_logic;
+			sloe : OUT std_logic;
+			pktend : OUT std_logic
 		);
 	END COMPONENT;
 
 
 	COMPONENT fifo_generator_v9_3
-	  PORT (
-		 rst : IN STD_LOGIC;
-		 wr_clk : IN STD_LOGIC;
-		 rd_clk : IN STD_LOGIC;
-		 din : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
-		 wr_en : IN STD_LOGIC;
-		 rd_en : IN STD_LOGIC;
-		 dout : OUT STD_LOGIC_VECTOR(15 DOWNTO 0);
-		 full : OUT STD_LOGIC;
-		 empty : OUT STD_LOGIC;
-		 valid : OUT STD_LOGIC
-	  );
+		PORT (
+			rst : IN STD_LOGIC;
+			wr_clk : IN STD_LOGIC;
+			rd_clk : IN STD_LOGIC;
+			din : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
+			wr_en : IN STD_LOGIC;
+			rd_en : IN STD_LOGIC;
+			dout : OUT STD_LOGIC_VECTOR(15 DOWNTO 0);
+			full : OUT STD_LOGIC;
+			empty : OUT STD_LOGIC;
+			valid : OUT STD_LOGIC
+		);
 	END COMPONENT;
-	
+
 	component clk_wiz_v3_6
-	port(
-		CLK_IN1 : in  std_logic;
-		CLK_OUT1: out std_logic;
-		CLK_OUT2: out std_logic;
-		CLK_OUT3: out std_logic;
-		CLK_OUT4: out std_logic;
-		RESET   : in  std_logic;
-		LOCKED  : out std_logic
-	);
+		port(
+			CLK_IN1 : in  std_logic;
+			CLK_OUT1: out std_logic;
+			CLK_OUT2: out std_logic;
+			CLK_OUT3: out std_logic;
+			CLK_OUT4: out std_logic;
+			RESET   : in  std_logic;
+			LOCKED  : out std_logic
+		);
 	end component;
 
 	-- declaracion de segnales
-	-- relojes
-		-- reloj usado en el sistema
-	signal sys_clk	: std_logic := '0';
-		-- salidas de pll
-	signal pll_50	: std_logic := '0';
-	signal pll_48	: std_logic := '0';
-	signal pll_40	: std_logic := '0';
-	signal pll_35	: std_logic := '0';
-	signal locked	: std_logic := '0';
+		-- relojes
+			-- reloj usado en el sistema
+	signal reloj_sistema:std_logic := '0';
+			-- salidas de pll
+	signal pll_50		:std_logic := '0';
+	signal pll_48		:std_logic := '0';
+	signal pll_40		:std_logic := '0';
+	signal pll_35		:std_logic := '0';
+	signal locked		:std_logic := '0';
 
 	-- cables de utilidad
-	signal write_req	: std_logic := '0';
-	signal slwr_sig	: std_logic := '0';
-	signal slrd_sig	: std_logic := '0';
-
---	-- cables para registro de eco
---	signal d_reg, q_reg											: std_logic_vector(port_width-1 downto 0);
---	signal c_reg, rst_reg, ce_reg								: std_logic;
+	signal enviar_datos	:std_logic := '0';
+	signal slwr_sig		:std_logic := '0';
+	signal slrd_sig		:std_logic := '0';
 
 	-- cables necesarios para la memoria fifo
-	signal fifo_full	: std_logic;
-	signal fifo_empty	: std_logic;
-	signal wr_en	: std_logic;
-	signal rd_en	: std_logic;
-	signal valid	: std_logic;
-	signal dout	: std_logic_vector(port_width-1 downto 0);
-	signal din	: std_logic_vector(port_width-1 downto 0);
+	signal fifo_llena	:std_logic;
+	signal fifo_vacia	:std_logic;
+	signal wr_en		:std_logic;
+	signal rd_en		:std_logic;
+	signal valid		:std_logic;
+	signal dout			:std_logic_vector(ancho_bus-1 downto 0);
+	signal din			:std_logic_vector(ancho_bus-1 downto 0);
 	-- segnales de sistema
 		--init
-	signal reset	: std_logic := '0';
+	signal reset	:std_logic := '0';
 
-		--para depuracion
---	signal debug_clk												: std_logic := '0';
---	signal counter													: std_logic_vector(15 downto 0) := x"0000";
---	signal checksum												: std_logic_vector(15 downto 0) := x"0000";
+	signal rst_cont	:natural range 0 to 1023 := 1023;
 
-		--temporizaciones
---	signal cont														: natural range 0 to 16777215 := 10000000;
-	signal rst_cont												: natural range 0 to 1023 := 1023;
-	
 	-- Maquinas de Estados: xc6slx9-2tqg144	
-	type fifo_wr_states is
-	(
-		idle, fifo_wren, fifo_nwren
+	type fifo_esc_estados is(
+		inicio, hab, dehab
 	);
-	signal fifo_wr_cst:	fifo_wr_states := idle;
-	signal fifo_wr_nst:	fifo_wr_states := idle;
-	
-	type fifo_rd_states is
-	(
-		idle, fifo_rden, fifo_nrden
-	);
-	signal fifo_rd_cst:	fifo_rd_states := idle;
-	signal fifo_rd_nst:	fifo_rd_states := idle;
-		
-	begin
+	signal fifo_esc_act:	fifo_esc_estados := inicio;
+	signal fifo_esc_prox:	fifo_esc_estados := inicio;
 
-	--debuggin leds
-		-- fisically connected. Don't comment!!!
+	type fifo_lec_estados is(
+		inicio, hab, dehab
+	);
+	signal fifo_lec_act:	fifo_lec_estados := inicio;
+	signal fifo_lec_prox:	fifo_lec_estados := inicio;
+
+begin
+
+	-- leds. Se conectan en la implementación física. 
+	-- NO COMENTAR!!!
 	led(7 downto 0) <= (others => '0');
-	
-	oddr_y : ODDR2 	                                           -- clk out buffer
-	port map
-	(
+
+	oddr_y : ODDR2	-- buffer para reloj salida 
+	port map(
 		D0 	=> '1',
 		D1 	=> '0',
 		CE 	=> '1',
@@ -169,13 +151,13 @@ architecture fx2lp_interface_arq of fx2lp_interface_top is
 		C1	=> (not pll_50),
 		R  	=> '0',
 		S  	=> '0',
-		Q  	=> clk_out
+		Q  	=> reloj_sal
 	);
 
 	--instanciaciones
 	-- interfaz
 	interface: fx2lp_interface PORT MAP(
-		clk => sys_clk,
+		reloj => reloj_sistema,
 		reset => reset,
 		fdata => fdata,
 		faddr => faddr,
@@ -187,154 +169,140 @@ architecture fx2lp_interface_arq of fx2lp_interface_top is
 		flagd => flagd,
 		sloe => sloe,
 		pktend => pktend,
-		send_req => write_req,
-		rx_data => din,
-		data_to_tx => dout
+		enviar_datos => enviar_datos,
+		dato_recibido => din,
+		dato_a_enviar => dout
 	);
 
 	--fifo
 	fifo : fifo_generator_v9_3
-	  PORT MAP (
-		 rst => not reset,
-		 wr_clk => sys_clk,
-		 rd_clk => sys_clk,
-		 din => din,
-		 wr_en => wr_en,
-		 rd_en => rd_en,
-		 dout => dout,
-		 full => fifo_full,
-		 empty => fifo_empty,
-		 valid => valid
-	  );
-	  
-	pll : clk_wiz_v3_6 
-		port map(
-			CLK_IN1   => clk_in,
-			CLK_OUT1  => pll_50,
-			CLK_OUT2  => pll_48,
-			CLK_OUT3  => pll_40,
-			CLK_OUT4  => pll_35,
-			RESET     => '0',
-			LOCKED    => locked
-		);
+	PORT MAP(
+		rst => not reset,
+		wr_clk => reloj_sistema,
+		rd_clk => reloj_sistema,
+		din => din,
+		wr_en => wr_en,
+		rd_en => rd_en,
+		dout => dout,
+		full => fifo_llena,
+		empty => fifo_vacia,
+		valid => valid
+	);
 
-	--REVISAR TODO ESTO
-	wr_req_ff : process (sys_clk, slwr_sig, fifo_empty)
+	pll : clk_wiz_v3_6 
+	port map(
+		CLK_IN1   => reloj_ent,
+		CLK_OUT1  => pll_50,
+		CLK_OUT2  => pll_48,
+		CLK_OUT3  => pll_40,
+		CLK_OUT4  => pll_35,
+		RESET     => '0',
+		LOCKED    => locked
+	);
+
+	env_sol_proc : process (reloj_sistema, slwr_sig, fifo_vacia)
 	begin
-		if rising_edge(sys_clk) then
-			if fifo_empty = '0' then
-				write_req <= '1';
+		if rising_edge(reloj_sistema) then
+			if fifo_vacia = '0' then
+				enviar_datos <= '1';
 			else
 				if slwr_sig = '1' then
-					write_req <= '0';
+					enviar_datos <= '0';
 				else
-					write_req <= write_req;
+					enviar_datos <= enviar_datos;
 				end if;
 			end if;
 		end if;
-	end process wr_req_ff;
-	--REVISAR TODO ESTO
-	
+	end process env_dat_proc;
+
 	-- reloj
-	sys_clk <= pll_48;
-	
+	reloj_sistema <= pll_48;
+
 	--conexiones de segnales internas hacia el exterior
 	slwr <= slwr_sig;
 	slrd <= slrd_sig;
-	
-	reset <= '1' when rst_cont = 0 else '0';
-	
-	-- fifo segnales
-	wr_en <= '1' when fifo_wr_cst = fifo_wren else '0';
 
-	rd_en <= '1' when fifo_rd_cst = fifo_rden else '0';
+	reset <=	'1' when rst_cont = 0 else
+				'1' when pulsador = '0' else '0';
+
+	-- fifo segnales
+	wr_en <= '1' when fifo_esc_act = dehab else '0';
+
+	rd_en <= '1' when fifo_lec_act = hab else '0';
 
 	-- maquina de estados de lectura fifo
-	read_fifo_fsm : process(fifo_rd_cst, fifo_empty, slwr_sig)
+	leer_fifo_mef : process(fifo_lec_act, fifo_vacia, slwr_sig)
 	begin
-		case fifo_rd_cst is
-			when idle =>
+		case fifo_lec_act is
+			when inicio =>
 				if slwr_sig = '0' then
-					if fifo_empty ='0' then
-						fifo_rd_nst <= fifo_rden;
+					if fifo_vacia ='0' then
+						fifo_lec_prox <= hab;
 					else
-						fifo_rd_nst <= idle;
+						fifo_lec_prox <= inicio;
 					end if;
 				else
-					fifo_rd_nst <= idle;
+					fifo_lec_prox <= inicio;
 				end if;
-					
-			when fifo_rden =>
-				fifo_rd_nst <= fifo_nrden;
-			
-			when fifo_nrden =>
+
+			when hab =>
+				fifo_lec_prox <= dehab;
+
+			when dehab =>
 				if slwr_sig ='1' then
-					fifo_rd_nst <= idle;
+					fifo_lec_prox <= inicio;
 				end if;
-				
+
 			when others =>
-				fifo_rd_nst <= idle;
+				fifo_lec_prox <= inicio;
 		end case;
-	end process read_fifo_fsm;
+	end process leer_fifo_mef;
 
 	-- maquina de estados escritura fifo
-	write_fifo_fsm: process(fifo_wr_cst,slrd_sig,fifo_full)
+	escr_fifo_mef: process(fifo_esc_act,slrd_sig,fifo_llena)
 	begin
-		case fifo_wr_cst is
-			when idle =>
+		case fifo_esc_act is
+			when inicio =>
 				if slrd_sig = '0' then
-					if fifo_full = '0' then
-						fifo_wr_nst <= fifo_wren;
+					if fifo_llena = '0' then
+						fifo_esc_prox <= hab;
 					else
-						fifo_wr_nst <= idle;
+						fifo_esc_prox <= inicio;
 					end if;
 				else
-					fifo_wr_nst <= idle;
+					fifo_esc_prox <= inicio;
 				end if;
-			
-			when fifo_wren =>
-				fifo_wr_nst <= fifo_nwren;
-				
-			when fifo_nwren =>
+
+			when hab =>
+				fifo_esc_prox <= dehab;
+
+			when dehab =>
 				if slrd_sig = '1' then
-				fifo_wr_nst <= idle;
+					fifo_esc_prox <= inicio;
 				end if;
-				
+
 			when others =>
-				fifo_wr_nst <= idle;
+				fifo_esc_prox <= inicio;
 		end case;
-	end process write_fifo_fsm;
-	
-	 -- reloj fsm
-	global_fsm_clk: process (sys_clk, reset)
+	end process escr_fifo_mef;
+
+	reloj_mef_global: process (reloj_sistema, reset)
 	begin
 		if reset = '0' then
-			fifo_rd_cst <= idle;
-			fifo_wr_cst <= idle;
-		elsif rising_edge(sys_clk) then
-			fifo_rd_cst <= fifo_rd_nst;
-			fifo_wr_cst <= fifo_wr_nst;
+			fifo_lec_act <= inicio;
+			fifo_esc_act <= inicio;
+		elsif rising_edge(reloj_sistema) then
+			fifo_lec_act <= fifo_lec_prox;
+			fifo_esc_act <= fifo_esc_prox;
 		end if;
-	end process global_fsm_clk;
+	end process reloj_mef_global;
 
---	reloj_lento: process(pll_0)
---	begin
---		if(rising_edge(pll_0))then
---			cont <= cont - 1;
---			if cont = 0 then
---				cont <= 10000000;
---				debug_clk <= not debug_clk;
---			end if;
---		end if;
---	end process reloj_lento;
-
-	init_rst: process(sys_clk)
+	inic_rst: process(reloj_sistema)
 	begin
 		if rst_cont /= 0 then
-			if rising_edge(sys_clk) then
+			if rising_edge(reloj_sistema) then
 				rst_cont <= rst_cont - 1;
 			end if;
 		end if;
-	end process init_rst;
-
+	end process inic_rst;
 end fx2lp_interface_arq;
--- docs/informe/secciones/extras/codigos/VHDL/top_tb.vhd
+++ docs/informe/secciones/extras/codigos/VHDL/top_tb.vhd
@@ -8,127 +8,113 @@ END interface_test_bench;
 ARCHITECTURE behavior OF interface_test_bench IS
 
 	-- Component Declaration for the Unit Under Test (UUT)
-
 	COMPONENT fx2lp_interface_top
-	PORT(
-		fdata : INOUT  std_logic_vector(15 downto 0);
-		faddr : OUT  std_logic_vector(1 downto 0);
-		slrd : OUT  std_logic;
-		slwr : OUT  std_logic;
-		flaga : IN  std_logic;
-		flagb : IN  std_logic;
-		flagc : IN  std_logic;
-		flagd : IN  std_logic;
-		sloe : OUT  std_logic;
-		pktend : OUT  std_logic;
-		clk_in : IN  std_logic;
-		clk_out : OUT  std_logic;
-		button : IN  std_logic;
-		led : OUT std_logic_vector(7 downto 0)
+		PORT(
+			fdata : INOUT  std_logic_vector(15 downto 0);
+			faddr : OUT  std_logic_vector(1 downto 0);
+			slrd : OUT  std_logic;
+			slwr : OUT  std_logic;
+			flaga : IN  std_logic;
+			flagb : IN  std_logic;
+			flagc : IN  std_logic;
+			flagd : IN  std_logic;
+			sloe : OUT  std_logic;
+			pktend : OUT  std_logic;
+			reloj_ent : IN  std_logic;
+			reloj_sal : OUT  std_logic;
+			pulsador : IN  std_logic;
+			led : OUT std_logic_vector(7 downto 0)
 		);
 	END COMPONENT;
 
 
-   --Inputs
-   signal flaga : std_logic := '0';
-   signal flagb : std_logic := '0';
-   signal flagc : std_logic := '0';
-   signal flagd : std_logic := '0';
-   signal clk_in : std_logic := '0';
-   signal button : std_logic := '0';
+	--Inputs
+	signal flaga : std_logic := '0';
+	signal flagb : std_logic := '0';
+	signal flagc : std_logic := '0';
+	signal flagd : std_logic := '0';
+	signal reloj_ent : std_logic := '0';
+	signal pulsador : std_logic := '0';
 
 	--BiDirs
-   signal fdata : std_logic_vector(15 downto 0);
+	signal fdata : std_logic_vector(15 downto 0);
 
- 	--Outputs
-   signal faddr : std_logic_vector(1 downto 0);
-   signal slrd : std_logic;
-   signal slwr : std_logic;
-   signal sloe : std_logic;
-   signal pktend : std_logic;
-   signal clk_out : std_logic;
+	--Outputs
+	signal faddr : std_logic_vector(1 downto 0);
+	signal slrd : std_logic;
+	signal slwr : std_logic;
+	signal sloe : std_logic;
+	signal pktend : std_logic;
+	signal reloj_sal : std_logic;
 	signal led : std_logic_vector(7 downto 0);
 
-   -- Clock period definitions
-   constant clk_in_period : time := 21 ns;
-   constant clk_out_period : time := 21 ns;
-	
+	-- Clock period definitions
+	constant periodo_reloj : time := 21 ns;
+
 	-- Data generator
-	signal counter: std_logic_vector(15 downto 0) := x"0000";
+	signal contador: std_logic_vector(15 downto 0) := x"0000";
 
 BEGIN
-
 	-- Instantiate the Unit Under Test (UUT)
-   uut: fx2lp_interface_top PORT MAP (
-          fdata => fdata,
-          faddr => faddr,
-          slrd => slrd,
-          slwr => slwr,
-          flaga => flaga,
-          flagb => flagb,
-          flagc => flagc,
-          flagd => flagd,
-          sloe => sloe,
-          pktend => pktend,
-          clk_in => clk_in,
-          clk_out => clk_out,
-          button => button,
-			 led => led			 
-        );
+	uut: fx2lp_interface_top PORT MAP (
+		fdata => fdata,
+		faddr => faddr,
+		slrd => slrd,
+		slwr => slwr,
+		flaga => flaga,
+		flagb => flagb,
+		flagc => flagc,
+		flagd => flagd,
+		sloe => sloe,
+		pktend => pktend,
+		reloj_ent => reloj_ent,
+		reloj_sal => reloj_sal,
+		pulsador => pulsador,
+		led => led			 
+	);
 
-   -- Clock process definitions
-   clk_in_process :process
-   begin
+	-- Clock process definitions
+	ent_reloj_process :process
+	begin
 		clk_in <= '0';
-		wait for clk_in_period/2;
+		wait for periodo_reloj/2;
 		clk_in <= '1';
-		wait for clk_in_period/2;
-   end process;
-
-   clk_out_process :process
-   begin
-		clk_out <= '0';
-		wait for clk_out_period/2;
-		clk_out <= '1';
-		wait for clk_out_period/2;
-   end process;
+		wait for periodo_reloj/2;
+	end process;
 
 	-- Start up reset
-	button <= '0', '1' after 100 ns;
+	pulsador <= '0', '1' after 100 ns;
 
-	-- flags signaling. All them are low_active
+	-- estimulos flags. todas activas en bajo
 	flags_proc: process
 	begin
 		flaga <= '1'; -- ep2full
 		flagb <= '0'; -- ep8empty
-		flagc	<= '1'; -- ep8full
+		flagc <= '1'; -- ep8full
 		flagd <= '0'; -- ep2empty
-		
 		wait for 5 us;
 
 		flagb <= '1';
 		wait until counter = x"0100";
 		wait until falling_edge(clk_in);
-		
+
 		flagb <= '0';
 		wait until fdata = x"00f0";
 		flaga <= '0';
 		wait until pktend = '0';
 		wait;
 	end process;
-	
-	-- Data counter
+
+	-- emulador de datos
 	data_proc: process(slrd)
 	begin
-		if button = '0' then
-			counter <= x"0000";
+		if pulsador = '0' then
+			contador <= x"0000";
 		elsif rising_edge(slrd)then
-			counter <= counter + 1;
+			contador <= contador + 1;
 		end if;
 	end process;
-	
-   -- Stimulus process
+
 	with flagb select
-		fdata <= counter when '1',(others => 'Z')when others;
-	
+		fdata <= contador when '1',(others => 'Z')when others;
 END;
